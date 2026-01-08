66d8dfdfed7290901f4fe2b35303c829aa16ab48
Hossein Pazouki <pazooki.h@gmail.com>
2026-01-08 15:43:02 +0100

    Add files via upload

---
+++ BuildWindow-git.cs
@@ -0,0 +1,26 @@
+using UnityEditor;
+using UnityEngine;
+
+public class BuildWindow : EditorWindow
+{
+    [MenuItem("Tools/Build Window")]
+    public static void Open()
+    {
+        GetWindow<BuildWindow>("Build");
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Build Actions", EditorStyles.boldLabel);
+
+        if (GUILayout.Button("Build Android"))
+        {
+            BuildScript.BuildAndroid();
+        }
+
+        if (GUILayout.Button("Build iOS"))
+        {
+            BuildScript.BuildiOS();
+        }
+    }
+}
